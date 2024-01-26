572fcb31ea73a424ec1f92b2d1bc75ed957a5ae2
Vandimion <vandi.mion04@gmail.com>
2024-01-26 18:59:17 +0800

    Added source tree and Blackjack.java

---
+++ src/blackjack/Blackjack.java
@@ -0,0 +1,9 @@
+package blackjack;
+
+public class Blackjack {
+
+
+    public static void main(String[] args) {
+        System.out.println("This is a console-based program for the game \"Blackjack.\"");
+    }
+}
